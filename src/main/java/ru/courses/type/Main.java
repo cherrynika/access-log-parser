335c93af56c880547bf9396a4510428f410155b3
Veronika Sukhonosova <qa.veronikas@gmail.com>
2024-11-05 21:21:31 +0300

    изменения

---
+++ src/main/java/ru/courses/type/Main.java
@@ -0,0 +1,28 @@
+package ru.courses.type;
+
+import ru.courses.geometry.Line;
+import ru.courses.geometry.Point;
+import ru.courses.geometry.PolyLine;
+import ru.courses.geometry.StringWrapper;
+
+public class Main {
+    public static void main(String[] args) {
+        Line line = new Line(5.0);
+        Point[] points = {new Point(0, 0), new Point(3, 4), new Point(7, 1)};
+        PolyLine polyLine = new PolyLine(points);
+        StringWrapper stringWrapper = new StringWrapper("Hello");
+
+        Measurable[] objects = {line, polyLine, stringWrapper};
+        double totalLength = calculateTotalLength(objects);
+
+        System.out.println("Результат: " + totalLength);
+    }
+
+    public static double calculateTotalLength(Measurable[] objects) {
+        double total = 0;
+        for (Measurable obj : objects) {
+            total += obj.getLength();
+        }
+        return total;
+    }
+}
